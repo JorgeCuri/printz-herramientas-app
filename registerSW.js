fa6a69347ae702582638183501e2f4991e23cd2c
Printz deploy <jcuri@printz.mx>
2026-07-14 12:05:11 -0600

    Deploy 0.7.0 (2026-07-14)

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/printz-herramientas-app/sw.js', { scope: '/printz-herramientas-app/' })})}
